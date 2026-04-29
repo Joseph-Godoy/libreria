7c4335229127ca0fb7729b2ee3e9f74994298add
Joseph Godoy <josephgodoy.t@gmail.com>
2026-04-28 23:45:38 -0600

    Fase beta, el programa ya funciona con lo minimo, faltan validaciones y estetica

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const miDialog = document.querySelector('#miDialog');
 const guardar = document.querySelector('#guardar');
 const lib = document.querySelector('.todos_libros');
 
-function crearLibro(tituloLibro, autorLibro, paginasLibro) {
+function crearLibro(tituloLibro, idLibro, autorLibro, leidoLibro, paginasLibro) {
     const seccion = document.querySelector('.todos_libros')
 
     const articulo = document.createElement('article');
     const imagen = document.createElement('img')
     const titulo = document.createElement('h3')
+    const id = document.createElement('p')
     const autor = document.createElement('h4')
     const paginas = document.createElement('p')
     const contenedero = document.createElement('div')
@@ -17,32 +18,50 @@ function crearLibro(tituloLibro, autorLibro, paginasLibro) {
     const checkbox = document.createElement('input')
     const boton = document.createElement('button')
 
-    articulo.classList.add('libro')
-    imagen.setAttribute('alt', tituloLibro)
-    titulo.textContent = tituloLibro
-    autor.textContent = autorLibro
-    paginas.textContent = `${paginasLibro} paginas`
-    label.textContent = 'Leido'
-    checkbox.setAttribute('type', 'checkbox')
-    contenedero.classList.add('grupo_boton')
-    boton.textContent = 'Borrar'
-    boton.classList.add('eliminar')
+    articulo.classList.add('libro');
+    imagen.setAttribute('alt', tituloLibro);
+    titulo.textContent = tituloLibro;
+    id.textContent = idLibro;
+    autor.textContent = autorLibro;
+    paginas.textContent = `${paginasLibro} paginas`;
+    label.textContent = 'Leido';
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', idLibro);
+    checkbox.setAttribute('class', 'checkboxLibro');
+    checkbox.checked = leidoLibro;
+    contenedero.classList.add('grupo_boton');
+    boton.textContent = 'Borrar';
+    boton.classList.add('eliminar');
+    boton.classList.add(idLibro);
 
     contenedero.appendChild(label)
     contenedero.appendChild(checkbox)
 
     articulo.appendChild(imagen)
     articulo.appendChild(titulo)
+    articulo.appendChild(id)
     articulo.appendChild(autor)
     articulo.appendChild(paginas)
     articulo.appendChild(contenedero)
     articulo.appendChild(boton)
 
     seccion.appendChild(articulo)
+
+    checkbox.addEventListener('change', () =>{
+    const libro = libreria.find(libro => libro.id === idLibro)
+    libro.leido = checkbox.checked
+});
+
+boton.addEventListener('click', () =>{
+    const index = libreria.findIndex(index => index.id === idLibro)
+    libreria.splice(index, 1);
+    render();
+});
 }
 
-function objetoLibros(titulo, autor, paginas, leido) {
+function objetoLibros(titulo, id, autor, leido, paginas) {
     this.titulo = titulo
+    this.id = id
     this.autor = autor
     this.paginas = paginas
     this.leido = leido
@@ -51,9 +70,8 @@ function objetoLibros(titulo, autor, paginas, leido) {
 guardar.addEventListener('click', () => {
     const datos = new FormData(document.querySelector('#formulario-principal'));
 
-    const objeto = new objetoLibros(datos.get('nombre'), datos.get('autor'), datos.get('paginas'))
-    alert(datos.get('leido'))
-    libreria.push(objeto)
+    const objeto = new objetoLibros(datos.get('nombre'), crypto.randomUUID(), datos.get('autor'), datos.get('paginas'));
+    libreria.push(objeto);
 
     render();
 
@@ -64,14 +82,16 @@ guardar.addEventListener('click', () => {
 function render() {
     lib.innerHTML = ''
     for (let i = 0; i < libreria.length; i++) {
-        crearLibro(libreria[i].titulo, libreria[i].autor, libreria[i].paginas)
+        crearLibro(libreria[i].titulo, libreria[i].id, libreria[i].autor, libreria[i].leido, libreria[i].paginas)
     }
 }
 
 
 
-const objeto1 = new objetoLibros('Harry Potter', 'J.K Rolling', '456 paginas')
-const objeto2 = new objetoLibros('El Arte de la Guerra', 'Tsun zu', '226 paginas')
+
+
+const objeto1 = new objetoLibros('Harry Potter', crypto.randomUUID(), 'J.K Rolling', true, '456 paginas')
+const objeto2 = new objetoLibros('El Arte de la Guerra', crypto.randomUUID(), 'Tsun zu', true,'226 paginas')
 
 libreria.push(objeto1)
 libreria.push(objeto2);
